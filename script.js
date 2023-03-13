4b2ec88a01ff5ace7c84778c8bbe6c84d2ae3755
Bonnzalino <siwakorn.paorod@gmail.com>
2023-03-13 13:52:39 +0700

    Add randomNumber 0-7

--- script.js
+++ script.js
@@ -1,25 +1,29 @@
-let dayNumber = '6';
+let randomNumber = Math.floor(Math.random() * 8);
+
+console.log(randomNumber);
+
+let dayNumber = randomNumber ;
 
 switch (dayNumber){
-    case '0':
+    case 0:
         console.log(`Sunday`);
         break;
-    case '1':
+    case 1:
         console.log(`Monday`);
         break;
-    case '2':
+    case 2:
         console.log(`Tuesday`);
         break;
-    case '3':
+    case 3:
         console.log(`Wednesday`);
         break;
-    case '4':
+    case 4:
         console.log(`Thursday`);
         break;
-    case '5':
+    case 5:
         console.log(`Friday`);
         break;
-    case '6':
+    case 6:
         console.log(`Saturday`);
         break;
     default:
